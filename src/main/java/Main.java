07d5db2194d831685789c8dcfdafe646f05283bd
RenatoAugusto17 <169068426+RenatoAugusto17@users.noreply.github.com>
2024-05-13 15:19:25 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("----------------------");
+    System.out.println("# Brooklyn Nine-Nine #");
+    System.out.println("----------------------");
+    System.out.println("");
+    System.out.println("O despreocupado detetive Jake Peralta precisa aprender a seguir as regras e trabalhar em equipe quando um capitão exigente assume o comando de seu esquadrão.");
+    System.out.println("");
+    System.out.println("Essa é uma série de Comédia Besteirol");
+    System.out.println("O link do Trailer:https://www.youtube.com/watch?v=sEOuJ4z5aTc");
+  }
+}
