b0a0ea67e5145406890d7ddc1ece005f225d6a9f
RenatoAugusto17 <169068426+RenatoAugusto17@users.noreply.github.com>
2024-05-13 21:21:11 +0000

    13/05/2024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,8 @@ public class Main {
     System.out.println("O despreocupado detetive Jake Peralta precisa aprender a seguir as regras e trabalhar em equipe quando um capitão exigente assume o comando de seu esquadrão.");
     System.out.println("");
     System.out.println("Essa é uma série de Comédia Besteirol");
-    System.out.println("O link do Trailer:https://www.youtube.com/watch?v=sEOuJ4z5aTc");
+    System.out.println("O link do Trailer:");
+    System.out.println("");
+    System.out.println("https://www.youtube.com/watch?v=sEOuJ4z5aTc");
   }
 }
